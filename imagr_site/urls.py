b64b4f804137bc5fae5bdac28e4763ed01b71164
Casey MacPhee <caseymacphee@gmail.com>
2014-11-21 09:42:22 -0800

    Change back the url so site root is at /imagr again

--- imagr_site/urls.py
+++ imagr_site/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^', include('imagr.urls', namespace='imagr')),
+    url(r'^imagr/', include('imagr.urls', namespace='imagr')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls'))
 )
